2e85648652b49259cfe5e01509a8b5d20b4347b9
Shizofreniya <mr.unionchik@gmail.com>
2021-06-30 20:01:58 +0300

    Add project files.

---
+++ LDBTokenParser/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+using LDBTokenParser.Association;
+
+namespace LDBTokenParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                var association = FileAssociations.SetAssociation(".ldb", "LdbTokenScrapper", "Discord LDB File",
+                    Path.Combine(Directory.GetCurrentDirectory(), Process.GetCurrentProcess().ProcessName + ".exe"));
+                Console.WriteLine(association
+                    ? "Формат файла .ldb был зарегистрирован как нужно."
+                    : "Похоже что этот тип файла уже зарегестрирован в системе... Просто нажми на 2 раза на LDB файл что бы его открыть через эту программу, или же запусти меня от имени администратора...");
+
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            var data = File.ReadAllText(args[0]);
+            var matches = Regex.Matches(data, @"(mfa\.[\w_\-]{84})|([\w]{24}\.[\w_\-]{6}\.[\w_\-]{27})");
+            switch (matches.Count)
+            {
+                case 0:
+                    Console.WriteLine("В этом файле нет токенов.");
+                    Console.ReadLine();
+                    break;
+                default:
+                {
+                    foreach (var match in matches)
+                    {
+                        Console.WriteLine($"Найден токен: {match}");
+                    }
+
+                    break;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+# LDBTokenScrapper
+Простая утилита для парсинга Discord токенов с LDB файлов.
+
+# Использование
+Качаем EXE файл с [Release](https://github.com/Lukumiya/LdbTokenScrapper/releases) вкладки и запускаем 1 раз!
+## ВАЖНО
+Не удаляйте и не перемещайте EXE файл, если вы его переместили - запустите заново, что бы он обновил пути
+
+# Если же вы хотите собрать сами
+Для этого нужно будет скачать .NET SDK и git, затем прописать эти команды:
+
+```sh
+$ git clone https://github.com/Lukumiya/LDBTokenScrapper.git
+$ cd LDBTokenScrapper
+$ dotnet build . -o ./build
+```
+Затем у вас в папке `build` будет exe файл, это и будет результатом сборки
+
+# Если ты сюда дошёл, можешь мне дать копеечку
+Вот сами реквизиты:
+
+ - Yoomoney: 4100116055781466
+ - Yoomoney Card: 40480 250 0015 2689 
+ - Qiwi Nickname: OKAYU
+ - BTC: bc1qmjwutjrpttc42yslzfdzn4wmgk68dtcgh7lw0l
+ - ETC: 0x1DF1Fa90Da17501920F57c146a133Cb3fb1C5804
+ - Lolzteam: [FubukiShirakami](https://lolz.guru/members/2818634/)
